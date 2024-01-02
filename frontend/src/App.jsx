26a33241595a70e1eae50c22f95895f9126c4a24
thrishank <thrishankkalluru@gmail.com>
2024-01-02 20:56:39 +0530

    App is fully working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,7 @@ import { Rendertodo } from "./components/Rendertodo";
 function App() {
   const [todos, setTodo] = useState([]);
 
-  fetch("http://localhost:3000/todos")
-  .then(async function (res) {
+  fetch("http://localhost:3000/todos").then(async function (res) {
     const json = await res.json();
     setTodo(json);
   });
--- frontend/src/components/Createtodo.jsx
+++ frontend/src/components/Createtodo.jsx
@@ -2,22 +2,42 @@ import { useState } from "react";
 
 export function Createtodo() {
   const [title, setTitle] = useState("");
+  const [notes, setNotes] = useState("");
+
   return (
     <div>
-      <input type="text" id="first" placeholder="Title" />
+      <input
+        type="text"
+        id="first"
+        placeholder="Title"
+        onChange={function (e) {
+          setTitle(e.target.value);
+        }}
+      />
       <br />
-      <input type="text" placeholder="Enter the description" />
+      <input
+        type="text"
+        placeholder="Enter the description"
+        onChange={function (e) {
+          setNotes(e.target.value);
+        }}
+      />
       <br />
       <button
         onClick={() => {
           fetch("http://localhost:3000/todos", {
             method: "POST",
-            body: {
-              // title: document.getElementById("first")
+            body: JSON.stringify({
+              title: title,
+              notes: notes,
+              isCompleted: false
+            }),
+            headers: {
+              "Content-type": "application/json",
             },
           }).then(async function (res) {
             const json = await res.json();
-            setTodos(json.todos);
+            // setTodos(json.todos);
           });
         }}
       >
